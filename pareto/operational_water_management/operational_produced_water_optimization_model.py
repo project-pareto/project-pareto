68b9cbd3597020a0877b9ff454f4206e0422e401
drouvenm <markus.drouven@netl.doe.gov>
2021-09-02 20:13:32 -0400

    revised freshwater sourcing cost constraint

--- pareto/operational_water_management/operational_produced_water_optimization_model.py
+++ pareto/operational_water_management/operational_produced_water_optimization_model.py
@@ -2349,7 +2349,7 @@ def create_model(df_sets, df_parameters, default={}):
     def TotalFreshSourcingCostRule(model):
         return model.v_C_TotalSourced == sum(
             sum(
-                sum(model.v_C_Sourced[f, p, t] for f in model.s_F if model.p_FCA[f, p])
+                sum(model.v_C_Sourced[f, p, t] for f in model.s_F)
                 for p in model.s_CP
             )
             for t in model.s_T
