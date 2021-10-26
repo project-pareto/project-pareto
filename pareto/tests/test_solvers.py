a5e74dfde64652150bd88f6f3252fc7bb3c2d822
Ludovico Bianchi <lbianchi@lbl.gov>
2021-10-26 11:22:49 -0700

    Run black

--- pareto/tests/test_solvers.py
+++ pareto/tests/test_solvers.py
@@ -103,7 +103,9 @@ def nonexisting_solver_name():
     return "bogus"
 
 
-@pytest.mark.skip("Solver validation and handling of non-existing solvers not yet implemented")
+@pytest.mark.skip(
+    "Solver validation and handling of non-existing solvers not yet implemented"
+)
 def test_nonexisting_solver_raises_error(nonexisting_solver_name):
     with pytest.raises(Exception, match=nonexisting_solver_name):
         solver = get_solver(nonexisting_solver_name)
