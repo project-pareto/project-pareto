804f83c0720f9bedafe53dc6ef55d3a05f8a132a
Andres Calderon <andresj_89@hotmail.com>
2021-11-09 09:50:26 -0500

    adding plotly as a dependency

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         # for the moment mainly for getting solvers with `idaes get-extensions`
         "idaes-pse",
         "requests",
+        "plotly"
     ],
     include_package_data=True,
     package_data={
