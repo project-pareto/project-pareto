f671ca68d598f44e089e57378f1ebc49c6792574
Ludovico Bianchi <lbianchi@lbl.gov>
2021-12-21 11:23:11 -0600

    Format stagedfright checks

--- .stagedfright/checks.py
+++ .stagedfright/checks.py
@@ -13,17 +13,22 @@ from stagedfright import StagedFile, AllowFile, PyContent
 
 
 def test_allowfile_matches_if_present(staged: StagedFile, allowfile: AllowFile):
-    assert staged.fingerprint == allowfile.fingerprint, f"An allowfile must contain a matching fingerprint for a staged file to be cleared for commit"
+    assert (
+        staged.fingerprint == allowfile.fingerprint
+    ), f"An allowfile must contain a matching fingerprint for a staged file to be cleared for commit"
 
 
-@pytest.mark.usefixtures('skip_if_matching_allowfile')
+@pytest.mark.usefixtures("skip_if_matching_allowfile")
 class TestIsClearedForCommit:
-
     def test_has_py_path_suffix(self, staged: StagedFile):
-        assert staged.suffix in {".py"}, "Only files with a 'py' extension may be cleared for commit"
+        assert staged.suffix in {
+            ".py"
+        }, "Only files with a 'py' extension may be cleared for commit"
 
     def test_is_text_file(self, staged: StagedFile):
-        assert staged.text_content is not None, "Only source (text) files may be cleared for commit"
+        assert (
+            staged.text_content is not None
+        ), "Only source (text) files may be cleared for commit"
 
     def test_has_meaningful_python_code(self, staged_pycontent: PyContent):
         assert len(staged_pycontent.ast_nodes.essential) >= 2
@@ -31,9 +36,12 @@ class TestIsClearedForCommit:
     @pytest.fixture
     def hardcoded_data_definitions(self, staged_pycontent: PyContent) -> List[ast.AST]:
         return [
-            n for n in staged_pycontent.ast_nodes.literal
+            n
+            for n in staged_pycontent.ast_nodes.literal
             if isinstance(n.value, numbers.Number) and n.value != 0
         ]
 
-    def test_py_module_does_not_contain_hardcoded_data(self, hardcoded_data_definitions: List[ast.AST]):
+    def test_py_module_does_not_contain_hardcoded_data(
+        self, hardcoded_data_definitions: List[ast.AST]
+    ):
         assert len(hardcoded_data_definitions) <= 2
