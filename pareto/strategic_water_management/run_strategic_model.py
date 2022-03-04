54e806c7d73075e52336b3eb58f14b892212f731
Andres Calderon <andresj_89@hotmail.com>
2022-03-04 15:37:14 -0500

    fixing black formatting

--- pareto/strategic_water_management/run_strategic_model.py
+++ pareto/strategic_water_management/run_strategic_model.py
@@ -95,7 +95,7 @@ parameter_list = [
 with resources.path(
     "pareto.case_studies",
     # "input_data_generic_strategic_case_study_LAYFLAT_FULL.xlsx"
-    "small_strategic_case_study.xlsx"
+    "small_strategic_case_study.xlsx",
 ) as fpath:
     [df_sets, df_parameters] = get_data(fpath, set_list, parameter_list)
 
