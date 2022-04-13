665cb8b2eda8fa2588b91da646cc5178f07daed1
melody-shellman <melody.shellman@ortec.com>
2022-04-13 17:34:45 -0400

    update test

--- pareto/tests/test_strategic_model.py
+++ pareto/tests/test_strategic_model.py
@@ -441,4 +441,4 @@ def test_run_reduced_strategic_model(build_reduced_strategic_model):
     assert results.solver.status == pyo.SolverStatus.ok
     assert degrees_of_freedom(m) == 63173
     # solutions obtained from running the reduced generic case study
-    assert pytest.approx(10188185.97, abs=1e-1) == pyo.value(m.v_Z)
+    assert pytest.approx(9751676.7, abs=1e-1) == pyo.value(m.v_Z)
