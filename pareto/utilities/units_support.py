23d978db4c284964ee6198f948a2b5f8fd416d9a
melody-shellman <melody.shellman@ortec.com>
2022-06-16 18:02:11 -0400

    add header

--- pareto/utilities/units_support.py
+++ pareto/utilities/units_support.py
@@ -1,3 +1,19 @@
+#####################################################################################################
+# PARETO was produced under the DOE Produced Water Application for Beneficial Reuse Environmental
+# Impact and Treatment Optimization (PARETO), and is copyright (c) 2021 by the software owners: The
+# Regents of the University of California, through Lawrence Berkeley National Laboratory, et al. All
+# rights reserved.
+#
+# NOTICE. This Software was developed under funding from the U.S. Department of Energy and the
+# U.S. Government consequently retains certain rights. As such, the U.S. Government has been granted
+# for itself and others acting on its behalf a paid-up, nonexclusive, irrevocable, worldwide license
+# in the Software to reproduce, distribute copies to the public, prepare derivative works, and perform
+# publicly and display publicly, and to permit other to do so.
+#####################################################################################################
+"""
+Provide PARETO specific support for units. This primarily supports test_strategic_model_unit_consistency
+in test_strategic_model.py
+"""
 from pyomo.core.expr.numvalue import (
     native_types,
     pyomo_constant_types,
