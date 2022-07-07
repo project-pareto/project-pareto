03c6d25df38a2bb12edb8df9a1083e19c9dc9ff2
Miguel Zamarripa-Perez <zamarrim@ad.science>
2022-07-07 13:29:36 -0400

    treatment option 1

--- pareto/strategic_water_management/run_strategic_model.py
+++ pareto/strategic_water_management/run_strategic_model.py
@@ -102,8 +102,8 @@ parameter_list = [
 # note the double backslashes '\\' in that path reference
 with resources.path(
     "pareto.case_studies",
-    "input_data_generic_strategic_case_study_LAYFLAT_FULL.xlsx",
-    # "small_strategic_case_study.xlsx",
+    # "input_data_generic_strategic_case_study_LAYFLAT_FULL.xlsx",
+    "small_strategic_case_study.xlsx",
 ) as fpath:
     [df_sets, df_parameters] = get_data(fpath, set_list, parameter_list)
 
@@ -130,7 +130,7 @@ options = {
     "scaling_factor": 1000,
     "running_time": 60,
     "gap": 0,
-    "water_quality": True,
+    "water_quality": False,
 }
 solve_model(model=strategic_model, options=options)
 
@@ -152,3 +152,160 @@ set_list = []
 parameter_list = ["v_F_Trucked", "v_C_Trucked"]
 fname = "strategic_optimization_results.xlsx"
 [sets_reports, parameters_report] = get_data(fname, set_list, parameter_list)
+
+strategic_model.v_F_UnusedTreatedWater.display()
+strategic_model.vb_y_Treatment.display()
+
+
+# ----------------------------------------------------------------------
+## adding treatment technologies model ##
+from pyomo.environ import (
+    Var,
+    Param,
+    Set,
+    ConcreteModel,
+    Constraint,
+    Objective,
+    minimize,
+    NonNegativeReals,
+    Reals,
+    Binary,
+    Any,
+    units as pyunits,
+    Block,
+    Suffix,
+    TransformationFactory,
+    value,
+)
+
+model = strategic_model
+
+model.s_DT = Set(initialize=["DTA", "DTB"], doc="Treatment Desalination Technologies")
+
+model.p_delta_DTreatment = Param(
+    model.s_DT,
+    default=pyunits.convert_value(
+        10,
+        from_units=pyunits.oil_bbl / pyunits.week,
+        to_units=model.model_units["volume_time"],
+    ),
+    initialize={"DTA": 50, "DTB": 90},
+    units=model.model_units["volume_time"],
+    doc="Treatment capacity installation/expansion increments [volume/time]",
+)
+
+model.vb_yw_Treatment = Var(
+    model.s_R,
+    model.s_DT,
+    within=Binary,
+    initialize=0,
+    doc="New or additional treatment capacity installed at treatment site with specific treatment capacity",
+)
+
+model.v_TW_Capacity = Var(
+    model.s_R,
+    within=NonNegativeReals,
+    units=model.model_units["volume_time"],
+    doc="Treatment capacity at a treatment site [volume/time]",
+)
+
+
+def TreatmentCapacityExpansionRule2(model, r):
+    return model.v_TW_Capacity[r] == model.p_sigma_Treatment[r] + sum(
+        model.p_delta_DTreatment[j] * model.vb_yw_Treatment[r, j] for j in model.s_DT
+    )
+
+
+model.TreatmentCapacityExpansion2 = Constraint(
+    model.s_R,
+    rule=TreatmentCapacityExpansionRule2,
+    doc="Treatment capacity construction/expansion",
+)
+
+# model.TreatmentCapacityExpansion.pprint()
+
+
+def TreatmentCapacityRule2(model, r, t):
+    return model.v_F_UnusedTreatedWater[r, t] <= model.v_TW_Capacity[r]
+
+    # sum(model.v_F_Piped[n, r, t] for n in model.s_N if model.p_NRA[n, r])
+    # + sum(model.v_F_Piped[s, r, t] for s in model.s_S if model.p_SRA[s, r])
+    # + sum(model.v_F_Trucked[p, r, t] for p in model.s_PP if model.p_PRT[p, r])
+    # + sum(model.v_F_Trucked[p, r, t] for p in model.s_CP if model.p_CRT[p, r])
+
+    # return process_constraint(constraint)
+
+
+model.TreatmentCapacity2 = Constraint(
+    model.s_R, model.s_T, rule=TreatmentCapacityRule2, doc="Treatment capacity"
+)
+
+# model.TreatmentCapacity.pprint()
+
+# def TreatmentBalanceRule(model, r, t):
+#     constraint = (
+#         model.p_epsilon_Treatment[r, model.p_W_TreatmentComponent[r]]
+#         * (
+#             sum(model.v_F_Piped[n, r, t] for n in model.s_N if model.p_NRA[n, r])
+#             + sum(model.v_F_Piped[s, r, t] for s in model.s_S if model.p_SRA[s, r])
+#             + sum(
+#                 model.v_F_Trucked[p, r, t] for p in model.s_PP if model.p_PRT[p, r]
+#             )
+#             + sum(
+#                 model.v_F_Trucked[p, r, t] for p in model.s_CP if model.p_CRT[p, r]
+#             )
+#         )
+#         == sum(model.v_F_Piped[r, p, t] for p in model.s_CP if model.p_RCA[r, p])
+#         + sum(model.v_F_Piped[r, s, t] for s in model.s_S if model.p_RSA[r, s])
+#         + model.v_F_UnusedTreatedWater[r, t]
+#     )
+#     return process_constraint(constraint)
+
+# model.TreatmentBalance = Constraint(
+#     model.s_R, model.s_T, rule=TreatmentBalanceRule, doc="Treatment balance"
+# )
+
+
+def LogicConstraintTreatmentRule2(model, r):
+    return sum(model.vb_yw_Treatment[r, j] for j in model.s_DT) == 1
+
+    # return process_constraint(constraint)
+
+
+model.LogicConstraintTreatment2 = Constraint(
+    model.s_R, rule=LogicConstraintTreatmentRule2, doc="Logic constraint treatment"
+)
+
+
+def TreatmentExpansionCapExRule2(model):
+    return model.v_C_TreatmentCapEx == sum(
+        sum(
+            model.vb_y_Treatment[r, j]
+            * model.p_kappa_Treatment[r, j]
+            * model.p_delta_Treatment[j]
+            for r in model.s_R
+        )
+        for j in model.s_J
+    ) + sum(
+        sum(
+            model.vb_yw_Treatment[r, j] * 1.4285 * model.p_delta_DTreatment[j]
+            for r in model.s_R
+        )
+        for j in model.s_DT
+    )
+    # return process_constraint(constraint)
+
+
+model.TreatmentExpansionCapEx = Constraint(
+    rule=TreatmentExpansionCapExRule2,
+    doc="Treatment construction or capacity expansion cost",
+)
+# solve_model(model=strategic_model, options=options)
+from pareto.utilities.solvers import get_solver, set_timeout
+
+solver = get_solver("gurobi_direct", "gurobi", "cbc")
+solver.solve(strategic_model, tee=True)
+
+strategic_model.v_F_UnusedTreatedWater.display()
+strategic_model.vb_y_Treatment.display()
+strategic_model.vb_yw_Treatment.display()
