492bd129cf342ce1106c7dd0f8b2989a1d5a25d2
melody-shellman <melody.shellman@ortec.com>
2022-12-14 14:52:10 -0500

    apply unified set to PipelineExpansionCapExDistanceBasedRule and PipelineExpansionCapExCapacityBasedRule

--- pareto/tests/test_strategic_model.py
+++ pareto/tests/test_strategic_model.py
@@ -158,7 +158,7 @@ def test_basic_build_capex_distance_based_capacity_input(build_strategic_model):
             "water_quality": WaterQuality.false,
         }
     )
-    assert degrees_of_freedom(m) == 56727
+    assert degrees_of_freedom(m) == 74782
     # Check unit config arguments
     assert len(m.config) == 6
     assert m.config.objective
@@ -181,7 +181,7 @@ def test_basic_build_capex_distance_based_capacity_calculated(build_strategic_mo
             "water_quality": WaterQuality.false,
         }
     )
-    assert degrees_of_freedom(m) == 56727
+    assert degrees_of_freedom(m) == 74782
     # Check unit config arguments
     assert len(m.config) == 6
     assert m.config.objective
@@ -204,7 +204,7 @@ def test_basic_build_capex_capacity_based_capacity_input(build_strategic_model):
             "water_quality": WaterQuality.false,
         }
     )
-    assert degrees_of_freedom(m) == 56727
+    assert degrees_of_freedom(m) == 74782
     # Check unit config arguments
     assert len(m.config) == 6
     assert m.config.objective
@@ -227,7 +227,7 @@ def test_basic_build_capex_capacity_based_capacity_calculated(build_strategic_mo
             "water_quality": WaterQuality.false,
         }
     )
-    assert degrees_of_freedom(m) == 56727
+    assert degrees_of_freedom(m) == 74782
     # Check unit config arguments
     assert len(m.config) == 6
     assert m.config.objective
@@ -321,7 +321,7 @@ def test_run_strategic_model(build_strategic_model):
     solver = get_solver("cbc")
     solver.options["seconds"] = 60
     results = solver.solve(m, tee=False)
-    assert degrees_of_freedom(m) == 56727
+    assert degrees_of_freedom(m) == 74782
 
 
 @pytest.fixture(scope="module")
@@ -435,7 +435,7 @@ def test_basic_reduced_build_capex_capacity_based_capacity_calculated(
             "water_quality": WaterQuality.false,
         }
     )
-    assert degrees_of_freedom(m) == 63715
+    assert degrees_of_freedom(m) == 67833
     # Check unit config arguments
     assert len(m.config) == 6
     assert m.config.objective
@@ -459,7 +459,7 @@ def test_basic_reduced_build_capex_capacity_based_capacity_input(
             "water_quality": WaterQuality.false,
         }
     )
-    assert degrees_of_freedom(m) == 63715
+    assert degrees_of_freedom(m) == 67833
     # Check unit config arguments
     assert len(m.config) == 6
     assert m.config.objective
@@ -483,7 +483,7 @@ def test_basic_reduced_build_capex_distance_based_capacity_input(
             "water_quality": WaterQuality.false,
         }
     )
-    assert degrees_of_freedom(m) == 63715
+    assert degrees_of_freedom(m) == 67833
     # Check unit config arguments
     assert len(m.config) == 6
     assert m.config.objective
@@ -507,7 +507,7 @@ def test_basic_reduced_build_discrete_water_quality_input(
             "water_quality": WaterQuality.discrete,
         }
     )
-    assert degrees_of_freedom(m) == 112075
+    assert degrees_of_freedom(m) == 116193
     # Check unit config arguments
     assert len(m.config) == 6
     assert m.config.objective
@@ -618,7 +618,7 @@ def test_run_reduced_strategic_model(build_reduced_strategic_model):
 
     assert results.solver.termination_condition == pyo.TerminationCondition.optimal
     assert results.solver.status == pyo.SolverStatus.ok
-    assert degrees_of_freedom(m) == 62415
+    assert degrees_of_freedom(m) == 66533
     # solutions obtained from running the reduced generic case study
     assert pytest.approx(32945.11, abs=1e-1) == pyo.value(m.v_Z)
 
