4df202bb44993952ad166882137bed6bb8872049
ElmiraShamlou <elmira.shamlou@keylogic.com>
2023-04-14 11:52:02 -0400

    renamed quality test functions for clarity

--- pareto/strategic_water_management/run_strategic_model.py
+++ pareto/strategic_water_management/run_strategic_model.py
@@ -117,7 +117,7 @@ parameter_list = [
 # note the double backslashes '\\' in that path reference
 with resources.path(
     "pareto.case_studies",
-    "strategic_small_case_study.xlsx",
+    "strategic_treatment_demo.xlsx",
 ) as fpath:
     [df_sets, df_parameters] = get_data(fpath, set_list, parameter_list)
 
