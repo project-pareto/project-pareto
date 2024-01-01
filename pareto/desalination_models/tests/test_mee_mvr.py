5da725b2fcfbc33e37c64a978fd2e8427846f0d6
Sakshi <73687517+Sakshi21299@users.noreply.github.com>
2024-01-01 15:56:37 +0530

    tests for mee-mvr model

---
+++ pareto/desalination_models/tests/test_mee_mvr.py
@@ -0,0 +1,90 @@
+import pytest
+import pyomo.environ as pyo
+from pareto.desalination_models.mee_mvr import make_mee_mvr_model
+
+ipopt_avail = pyo.SolverFactory("ipopt").available()
+
+class TestMeeMvrModel():
+    @pytest.mark.skipif(not ipopt_avail, reason="ipopt is not available")
+    def test_single_stage(self):
+        m = make_mee_mvr_model(N_evap = 1, inputs_variables=False)
+        m.flow_feed = 10 
+        m.salt_feed = 70
+        
+        ipopt = pyo.SolverFactory('ipopt')
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 10 
+        m.salt_feed = 140
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 20
+        m.salt_feed = 140
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 20
+        m.salt_feed = 70
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+      
+    @pytest.mark.skipif(not ipopt_avail, reason="ipopt is not available")
+    def test_two_stage(self):
+        m = make_mee_mvr_model(N_evap = 2, inputs_variables=False)
+        m.flow_feed = 10 
+        m.salt_feed = 70
+        
+        ipopt = pyo.SolverFactory('ipopt')
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 10 
+        m.salt_feed = 140
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 20
+        m.salt_feed = 140
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 20
+        m.salt_feed = 70
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+    @pytest.mark.skipif(not ipopt_avail, reason="ipopt is not available")
+    def test_three_stage(self):
+        m = make_mee_mvr_model(N_evap = 3, inputs_variables=False)
+        #This needs a higher compression ratio to find the optimum
+        m.CR_max = 10
+        
+        m.flow_feed = 10 
+        m.salt_feed = 70
+        
+        ipopt = pyo.SolverFactory('ipopt')
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 10 
+        m.salt_feed = 140
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 20
+        m.salt_feed = 140
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        m.flow_feed = 20
+        m.salt_feed = 70
+        res = ipopt.solve(m)
+        pyo.assert_optimal_termination(res)
+        
+        
+        
+if __name__ == "__main__":
+    pytest.main()
+        
