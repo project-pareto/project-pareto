0c5fb063499556b79608ae513dbc855abe6c176c
Sakshi <73687517+Sakshi21299@users.noreply.github.com>
2024-01-01 16:28:40 +0530

    add tests to check dof

--- pareto/desalination_models/tests/test_mee_mvr.py
+++ pareto/desalination_models/tests/test_mee_mvr.py
@@ -1,10 +1,21 @@
 import pytest
 import pyomo.environ as pyo
 from pareto.desalination_models.mee_mvr import make_mee_mvr_model
+from idaes.core.util.model_statistics import degrees_of_freedom
 
 ipopt_avail = pyo.SolverFactory("ipopt").available()
 
 class TestMeeMvrModel():
+    def test_dof(self):
+        m = make_mee_mvr_model(N_evap = 1, inputs_variables=False)
+        assert degrees_of_freedom(m) == 2
+        
+        m = make_mee_mvr_model(N_evap = 2, inputs_variables=False)
+        assert degrees_of_freedom(m) == 3
+        
+        m = make_mee_mvr_model(N_evap = 3, inputs_variables=False)
+        assert degrees_of_freedom(m) == 5
+    
     @pytest.mark.skipif(not ipopt_avail, reason="ipopt is not available")
     def test_single_stage(self):
         m = make_mee_mvr_model(N_evap = 1, inputs_variables=False)
@@ -87,4 +98,3 @@ class TestMeeMvrModel():
         
 if __name__ == "__main__":
     pytest.main()
-        
