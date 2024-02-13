ee9a88e469c9f2452bec2ecbbbd70fb50cc60a0b
melody-shellman <melody.shellman@ortec.com>
2024-02-13 12:10:50 -0500

    run black

--- pareto/tests/test_mee_mvr.py
+++ pareto/tests/test_mee_mvr.py
@@ -18,96 +18,96 @@ from idaes.core.util.model_statistics import degrees_of_freedom
 
 ipopt_avail = pyo.SolverFactory("ipopt").available()
 
-class TestMeeMvrModel():
+
+class TestMeeMvrModel:
     def test_dof(self):
-        m = make_mee_mvr_model(N_evap = 1, inputs_variables=False)
+        m = make_mee_mvr_model(N_evap=1, inputs_variables=False)
         assert degrees_of_freedom(m) == 2
-        
-        m = make_mee_mvr_model(N_evap = 2, inputs_variables=False)
+
+        m = make_mee_mvr_model(N_evap=2, inputs_variables=False)
         assert degrees_of_freedom(m) == 3
-        
-        m = make_mee_mvr_model(N_evap = 3, inputs_variables=False)
+
+        m = make_mee_mvr_model(N_evap=3, inputs_variables=False)
         assert degrees_of_freedom(m) == 5
-    
+
     @pytest.mark.skipif(not ipopt_avail, reason="ipopt is not available")
     def test_single_stage(self):
-        m = make_mee_mvr_model(N_evap = 1, inputs_variables=False)
-        m.flow_feed = 10 
+        m = make_mee_mvr_model(N_evap=1, inputs_variables=False)
+        m.flow_feed = 10
         m.salt_feed = 70
-        
-        ipopt = pyo.SolverFactory('ipopt')
+
+        ipopt = pyo.SolverFactory("ipopt")
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
-        m.flow_feed = 10 
+
+        m.flow_feed = 10
         m.salt_feed = 140
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
+
         m.flow_feed = 20
         m.salt_feed = 140
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
+
         m.flow_feed = 20
         m.salt_feed = 70
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-      
+
     @pytest.mark.skipif(not ipopt_avail, reason="ipopt is not available")
     def test_two_stage(self):
-        m = make_mee_mvr_model(N_evap = 2, inputs_variables=False)
+        m = make_mee_mvr_model(N_evap=2, inputs_variables=False)
         m.flow_feed = 10
         m.salt_feed = 70
-        
-        ipopt = pyo.SolverFactory('ipopt')
+
+        ipopt = pyo.SolverFactory("ipopt")
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
-        m.flow_feed = 10 
+
+        m.flow_feed = 10
         m.salt_feed = 140
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
+
         m.flow_feed = 20
         m.salt_feed = 140
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
+
         m.flow_feed = 20
         m.salt_feed = 70
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
+
     @pytest.mark.skipif(not ipopt_avail, reason="ipopt is not available")
     def test_three_stage(self):
-        m = make_mee_mvr_model(N_evap = 3, inputs_variables=False)
-        #This needs a higher compression ratio to find the optimum
+        m = make_mee_mvr_model(N_evap=3, inputs_variables=False)
+        # This needs a higher compression ratio to find the optimum
         m.CR_max = 6
-        
+
         m.flow_feed = 10
         m.salt_feed = 70
-        
-        ipopt = pyo.SolverFactory('ipopt')
+
+        ipopt = pyo.SolverFactory("ipopt")
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
-        m.flow_feed = 10 
+
+        m.flow_feed = 10
         m.salt_feed = 140
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
+
         m.flow_feed = 20
         m.salt_feed = 140
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
+
         m.flow_feed = 20
         m.salt_feed = 70
         res = ipopt.solve(m)
         pyo.assert_optimal_termination(res)
-        
-        
-        
+
+
 if __name__ == "__main__":
     pytest.main()
