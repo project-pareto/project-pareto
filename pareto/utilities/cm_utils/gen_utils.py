a68cbafe05aa0c94a38a8353d6531af6de3cb4a3
Arsh Bhatia <arshbhatia2001@gmail.com>
2024-03-06 14:37:07 -0500

    Screening tool added.

---
+++ pareto/utilities/cm_utils/gen_utils.py
@@ -0,0 +1,185 @@
+"""
+General Utilities
+This file contains any utility functions that would help processing data,
+generating reports, etc.
+"""
+
+import pyomo.environ as pyo
+import pandas as pd
+import sys
+
+def set_processing(d):
+    # This function does any required processing for the sets
+    if 's_NT' in d:
+        d['s_NTIN'] = []
+        d['s_NTTW'] = []
+        d['s_NTCW'] = []
+        for i in d['s_NT']:
+            d['s_NTIN'].append(i+'IN')
+            d['s_NTTW'].append(i+'TW')
+            d['s_NTCW'].append(i+'CW')
+
+    d['s_N'] = (d['s_NMS'] + d['s_NP'] + d['s_NC'] + d['s_NS'] + d['s_NW'] 
+                + d['s_ND'] + d['s_NTIN'] + d['s_NTTW'] + d['s_NTCW'])
+
+    # Categorized Nodes
+    d['s_NNoInv'] = d['s_NMS'] + d['s_NP'] + d['s_NC']
+    d['s_NInv'] = d['s_NS']
+    d['s_NSrc'] = d['s_NW']
+    d['s_NSnk'] = d['s_ND'] + d['s_NTIN']
+    d['s_NTSrc'] = d['s_NTTW'] + d['s_NTCW']
+
+    # Arcs In
+    A_out = dict()
+    A_in = dict()
+
+    items = [A_out, A_in]
+    for i in range(len(items)):
+        for n in d['s_N']:
+            n_tuple_set = [] 
+            for arc in d['s_A']:
+                if n == arc[i]:
+                    n_tuple_set.append(arc)
+            items[i][n] = n_tuple_set
+    
+    d['s_Ain'] = A_in
+    d['s_Aout'] = A_out
+
+    return d
+
+def parameter_processing(d):
+    # This function is required for processing the parameters
+
+    d['p_nodeUp'] = {a: a[0] for a in d['s_A']}
+    d['p_nodeDown'] = {a: a[1] for a in d['s_A']}
+    d['p_tp'] = {a: a[2] for a in d['s_A']}
+
+    return d
+
+
+
+def data_preprocessing(d):
+    # TODO: assert in data preprocessing that there should not be demand and flowback for a completion pad during the same t 
+    # TODO: Assert that if there is no flow coming out of a completion pad, there should not be a composition either
+    # TODO: Handle empty sets
+    # TODO: Assert that arcs are made by components that exist in s_N
+    
+    # creating a set N: union of all nodes
+    d['s_N'] = d['s_NMS'] + d['s_NP'] + d['s_NC'] + d['s_NS'] + d['s_ND']+ d['s_NW'] + d['s_NT']
+
+    # defining input and output arcs
+    A_out = dict()
+    A_in = dict()
+
+    items = [A_out, A_in]
+    for i in range(len(items)):
+        for n in d['s_N']:
+            n_tuple_set = [] 
+            for arc in d['s_A']:
+                if n == arc[i]:
+                    n_tuple_set.append(arc)
+            items[i][n] = n_tuple_set
+
+    d['s_Ain'] = A_in
+    d['s_Aout'] = A_out 
+
+    return d
+
+def solve_nlp(m, solvername='gurobi', time_limit=100, tee=True):
+    if solvername == 'gurobi':
+        opt = pyo.SolverFactory('gurobi')
+        opt.options['TimeLimit'] = time_limit
+        opt.options['NonConvex'] = 2
+        # opt.options['Threads'] = 8
+        m.results = opt.solve(m, tee=tee)
+    
+    # TODO: Add flags needed for opensource version of ipopt
+    # elif solvername == 'ipopt' ...
+
+    else:
+        # SOLVE
+        opt = pyo.SolverFactory('gams', solver=solvername)
+        m.results = opt.solve(m, tee=tee,
+                            add_options=[
+                                'option reslim = ',str(time_limit),';'])
+    
+    return m
+
+def report_results_to_excel(model: pyo.ConcreteModel, filename: str, split_var: dict = {}) -> None:
+    counter = 0
+    for var_name, var in model.component_map(pyo.Var, active=True).items():
+        
+        #creating the columns for the excel file
+        set_names = []
+        for index_set in var.index_set().subsets(expand_all_set_operators=False):
+            if index_set.name in list(split_var.keys()):
+                num_splits = split_var[index_set.name]
+                for i in range(1,num_splits+1):
+                    set_names.append(index_set.name + str(i))
+            else:
+                set_names.append(index_set.name)
+        set_names.append('Values')
+
+        # assigning values to the data frame
+        df = pd.DataFrame(columns = set_names)
+        k = 0
+        for i in list(var.keys()):
+            row = list(i)
+            row.append(var[i].value)
+            df.loc[k] = row
+            k += 1
+
+        # adding variable sheet to the excel
+        if counter == 0:
+            with pd.ExcelWriter(filename, mode = 'w') as writer: 
+                df.to_excel(writer, sheet_name = var.name)
+        else:
+            with pd.ExcelWriter(filename, mode = 'a') as writer: 
+                df.to_excel(writer, sheet_name = var.name)
+        counter += 1
+
+# Function to turn off certain variables and constraints
+def obj_fix(model: pyo.ConcreteModel, vars = [], activate = [], deactivate = []):
+
+    # Fixing and unfixing specified variables
+    for var_name, var in model.component_map(pyo.Var, active = True).items():
+        # fixing variables if they are specified by user
+        if var_name in vars:
+            for i in list(var.keys()):
+                var[i].fix()
+        # unfixing remaining variables
+        else:
+            var.unfix()
+
+    # Deactivating constraints specified by user
+    # for con_name, con in model.component_map(pyo.Constraint, active = True).items():
+    #     if con_name in cons:    # deactivating
+    #         con.deactivate()
+    #     else:   # activating 
+    #         print(con_name)
+    #         con.activate()
+
+    for con in activate:
+        con.activate()
+    for con in deactivate:
+        con.deactivate()
+    
+    return model
+
+def alter_nonlinear_cons(model:pyo.ConcreteModel, deactivate=False):
+    for con in model.component_data_objects(ctype=pyo.Constraint, descend_into=True):
+        degree = con.body.polynomial_degree()
+        if degree != 0 and degree != 1:
+            assert degree == 2
+            con.deactivate() if deactivate else con.activate()
+
+    return model
+
+# Terminate script if model is infeasible or unbounded
+def terminate(status):
+    term_cond = status.solver.termination_condition
+    if  term_cond == 'infeasible':
+        print("Model is", term_cond, ". Terminating script")
+        return sys.exit()
+    else:
+        pass
---
+++ pareto/utilities/cm_utils/opt_utils.py
@@ -0,0 +1,185 @@
+from pareto.CM_module.models.qcp5 import build_qcp5
+from pareto.CM_module.models.qcp_br import build_qcp_br
+import pyomo.environ as pyo
+from pareto.utilities.cm_utils.gen_utils import obj_fix
+
+def max_theoretical_recovery_flow(model, treatment_unit, desired_li_conc):
+    """ This function computes the largest flow possible to
+    the treatment unit while still keeping the Li
+    concentration above the desired level.
+
+    This function ignores all infrastructure - this is only
+    based on mass balance along.
+    """
+    # get the efficiency for the treatment unit
+    assert model.p_alpha[treatment_unit, 'Li'] > 0.999
+    alphaW = model.p_alphaW[treatment_unit]
+    # get the desired li conc at the inlet - correct this later
+    desired_li_conc = desired_li_conc*(1-alphaW)
+
+    # create a list of the flows and concentrations
+    # sort by largest concentration first
+    produced_flows_conc = list()
+    sumf = 0
+    for t in model.p_FGen:
+        f = pyo.value(model.p_FGen[t])*7 # convert to bbls/wk
+        sumf += f
+        c = pyo.value(model.p_CGen[t[0], 'Li', t[1]])
+        produced_flows_conc.append((f,c))
+    produced_flows_conc.sort(key=lambda t: t[1], reverse=True)
+
+    # iterate through the flows and accumulate until the
+    # overall concentration goes below the desired limit
+    cumulative_f = 0
+    cumulative_li = 0
+    li_conc = 0
+    for f,c in produced_flows_conc:
+        cf = cumulative_f + f
+        cli = cumulative_li + f*c
+        li_conc = cli/cf
+        # print(f, c, cf, li_conc)
+        if li_conc > desired_li_conc:
+            cumulative_f = cf
+            cumulative_li = cli
+        else:
+            ff = (cumulative_li - desired_li_conc*cumulative_f)/(desired_li_conc - c)
+            # print('***', ff)
+            cumulative_f += ff
+            cumulative_li += ff*c
+            li_conc = cumulative_li/cumulative_f
+            break
+
+    return cumulative_f*(1-alphaW)
+
+def max_theoretical_recovery_flow_opt(model, treatment_unit, desired_li_conc, tee=False):
+    """ This function computes the largest flow possible to
+    the treatment unit while still keeping the Li
+    concentration above the desired level.
+
+    This function ignores all infrastructure - this is only
+    based on mass balance along.
+
+    This function differs from max_theoretical_recovery_flow
+    since this function uses an LP and Gurobi to find the value
+    """
+    assert model.p_alpha[treatment_unit, 'Li'] > 0.999
+    alphaW = model.p_alphaW[treatment_unit]
+    # desired concentration at the inlet - this will be corrected at the end
+    desired_li_conc = desired_li_conc*(1-alphaW) 
+
+    mm = pyo.ConcreteModel()
+    mm.S = list(model.p_FGen.index_set())
+    bounds = {k: (0,model.p_FGen[k]*7) for k in mm.S}
+    mm.F = pyo.Var(mm.S, bounds=bounds)
+    mm.cumulative_F = pyo.Var(bounds=(10,None))
+    mm.cumulative_F_con = \
+        pyo.Constraint(expr = mm.cumulative_F == sum(mm.F[t] for t in mm.S) )
+    mm.obj = pyo.Objective(expr = mm.cumulative_F, sense=pyo.maximize)
+    mm.total_li = \
+        pyo.Expression(expr = sum(mm.F[t]*model.p_CGen[t[0],'Li',t[1]] for t in mm.S))
+    mm.quality_con = \
+        pyo.Constraint(expr = mm.total_li >= desired_li_conc*mm.cumulative_F)
+    # mm.overall_conc = pyo.Expression(expr = mm.total_li/sum(mm.F[t] for t in mm.S))
+    
+    status = pyo.SolverFactory('gurobi').solve(mm, tee=tee)
+    pyo.assert_optimal_termination(status)
+    return pyo.value(mm.cumulative_F)*(1-alphaW)
+
+def max_recovery_with_infrastructure(data, tee=False):
+    # build the model from the loaded data
+    model = build_qcp_br(data)
+
+    ###
+    # First, we solve a flow-based LP without any concentration variables
+    ###
+    model.obj.deactivate()
+    model.br_obj.deactivate()
+    model.treatment_only_obj.activate()
+
+    # create lists of constraints involving concentrations and all flow / inventory variables
+    conclist = [model.MSconc, model.Pconc, model.Cconc, model.Sconc, model.Wconc, \
+                model.TINconc, model.NTTWconc, model.NTCWconc, model.NTSrcconc, model.minconccon]
+    flowlist = [model.Cflow, model.noinvflow, model.Sinv, model.Dflow, model.Wflow, model.TINflow, \
+                model.NTTWflow, model.NTCWflow]
+
+    # I think this fixes the concentration variables and removes the concentration constraints
+    # TODO: Why is this called obj_fix? what is "obj"?
+    model = obj_fix(model, ['v_C'], activate = flowlist, deactivate = conclist)
+
+    # Solve the linear flow model
+    print("   ... running linear flow model")
+    opt = pyo.SolverFactory('gurobi')
+    #opt.options['NonConvex'] = 2
+    #opt.options['TimeLimit'] = 150
+    status = opt.solve(model, tee=tee)
+
+    # terminating script early if optimal solution not found
+    pyo.assert_optimal_termination(status)
+
+    ###
+    # Bilinear NLP
+    ###
+
+    # unfixing all the initialized variables
+    model = obj_fix(model, [], deactivate=[], activate=conclist+flowlist)
+
+
+    # solve for the maximum possible recovery revenue given this infrastructure
+    model.TINflow.deactivate()
+    for k in model.Dflow:
+        if k[0] == 'K1_TW' or k[0] == 'K1_CW':
+            model.Dflow[k].deactivate()
+
+    # running bilinear model
+    print("   ... running bilinear model")
+    opt = pyo.SolverFactory('ipopt')
+    opt.options['ma27_pivtol'] = 1e-2
+    opt.options['tol'] = 1e-6
+    status = opt.solve(model, tee=tee)
+    pyo.assert_optimal_termination(status)
+    print("Max lithium revenue with existing infrastructure:", pyo.value(model.treat_rev))
+    return model
+
+def cost_optimal(data, tee=False):
+    # build the model from the loaded data
+    model = build_qcp_br(data)
+
+    ###
+    # First, we solve a flow-based LP without any concentration variables
+    ###
+
+    # create lists of constraints involving concentrations and all flow / inventory variables
+    conclist = [model.MSconc, model.Pconc, model.Cconc, model.Sconc, model.Wconc, \
+                model.TINconc, model.NTTWconc, model.NTCWconc, model.NTSrcconc, model.minconccon]
+    flowlist = [model.Cflow, model.noinvflow, model.Sinv, model.Dflow, model.Wflow, model.TINflow, \
+                model.NTTWflow, model.NTCWflow]
+
+    # I think this fixes the concentration variables and removes the concentration constraints
+    # TODO: Why is this called obj_fix? what is "obj"?
+    model = obj_fix(model, ['v_C'], activate = flowlist, deactivate = conclist)
+
+    # Solve the linear flow model
+    print("   ... running linear flow model")
+    opt = pyo.SolverFactory('gurobi')
+    #opt.options['NonConvex'] = 2
+    #opt.options['TimeLimit'] = 150
+    status = opt.solve(model, tee=tee)
+
+    # terminating script early if optimal solution not found
+    pyo.assert_optimal_termination(status)
+
+    ###
+    # Bilinear NLP
+    ###
+
+    # unfixing all the initialized variables
+    model = obj_fix(model, [], deactivate=[], activate=conclist+flowlist)
+
+
+    # running bilinear model
+    print("   ... running bilinear model")
+    opt = pyo.SolverFactory('ipopt')
+    opt.options['max_iter'] = 10000
+    status = opt.solve(model, tee=tee)
+    pyo.assert_optimal_termination(status)
+    return model
