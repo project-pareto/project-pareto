3b96c7599c02f4f328ec852689fec34efed275ac
Ludovico Bianchi <lbianchi@lbl.gov>
2024-03-15 10:35:43 -0500

    Add upper version constraint for pandas

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     py_modules=["stagedfright"],
     install_requires=[
         "pyomo>=6.2",
-        "pandas>=2",
+        "pandas>=2,<3",
         "openpyxl",
         # for the moment mainly for getting solvers with `idaes get-extensions`
         # https://peps.python.org/pep-0440/#compatible-release
