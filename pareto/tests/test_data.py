a192c5f7cab3c6b4fac3eb94d6dd9feecf853d98
Travis Arnold <travis.arnold@netl.doe.gov>
2024-06-04 12:16:35 -0500

    Update test_data.py to use new version of get_data()

--- pareto/tests/test_data.py
+++ pareto/tests/test_data.py
@@ -16,168 +16,6 @@ import pytest
 from pareto.utilities.get_data import get_data
 
 
-class Sets:
-    operational = [
-        "ProductionPads",
-        "CompletionsPads",
-        "ProductionTanks",
-        "ExternalWaterSources",
-        "WaterQualityComponents",
-        "StorageSites",
-        "SWDSites",
-        "TreatmentSites",
-        "ReuseOptions",
-        "NetworkNodes",
-    ]
-    strategic = [
-        "ProductionPads",
-        "CompletionsPads",
-        "SWDSites",
-        "ExternalWaterSources",
-        "WaterQualityComponents",
-        "StorageSites",
-        "TreatmentSites",
-        "ReuseOptions",
-        "NetworkNodes",
-        "PipelineDiameters",
-        "StorageCapacities",
-        "InjectionCapacities",
-        "TreatmentCapacities",
-        "TreatmentTechnologies",
-    ]
-
-
-class Parameters:
-    operational = [
-        "Units",
-        "RCA",
-        "FCA",
-        "PCT",
-        "FCT",
-        "CCT",
-        "PKT",
-        "PRT",
-        "CKT",
-        "CRT",
-        "PAL",
-        "CompletionsDemand",
-        "PadRates",
-        "TankFlowbackRates",
-        "FlowbackRates",
-        "ProductionTankCapacity",
-        "DisposalCapacity",
-        "CompletionsPadStorage",
-        "TreatmentCapacity",
-        "ExtWaterSourcingAvailability",
-        "PadOffloadingCapacity",
-        "TruckingTime",
-        "DisposalOperationalCost",
-        "TreatmentOperationalCost",
-        "ReuseOperationalCost",
-        "PadStorageCost",
-        "PipelineOperationalCost",
-        "TruckingHourlyCost",
-        "ExternalSourcingCost",
-        "ProductionRates",
-        "TreatmentEfficiency",
-        "ExternalWaterQuality",
-        "PadWaterQuality",
-        "StorageInitialWaterQuality",
-    ]
-    strategic = [
-        "Units",
-        "PNA",
-        "CNA",
-        "CCA",
-        "NNA",
-        "NCA",
-        "NKA",
-        "NRA",
-        "NSA",
-        "FCA",
-        "RCA",
-        "RNA",
-        "RSA",
-        "SCA",
-        "SNA",
-        "ROA",
-        "RKA",
-        "SOA",
-        "NOA",
-        "PCT",
-        "PKT",
-        "FCT",
-        "CST",
-        "CCT",
-        "CKT",
-        "RST",
-        "ROT",
-        "SOT",
-        "RKT",
-        "Elevation",
-        "CompletionsPadOutsideSystem",
-        "DesalinationTechnologies",
-        "DesalinationSites",
-        "BeneficialReuseCost",
-        "BeneficialReuseCredit",
-        "TruckingTime",
-        "CompletionsDemand",
-        "PadRates",
-        "FlowbackRates",
-        "WellPressure",
-        "NodeCapacities",
-        "InitialPipelineCapacity",
-        "InitialPipelineDiameters",
-        "InitialDisposalCapacity",
-        "InitialTreatmentCapacity",
-        "ReuseMinimum",
-        "ReuseCapacity",
-        "ExtWaterSourcingAvailability",
-        "PadOffloadingCapacity",
-        "CompletionsPadStorage",
-        "DisposalOperationalCost",
-        "TreatmentOperationalCost",
-        "ReuseOperationalCost",
-        "PipelineOperationalCost",
-        "ExternalSourcingCost",
-        "TruckingHourlyCost",
-        "PipelineDiameterValues",
-        "DisposalCapacityIncrements",
-        "InitialStorageCapacity",
-        "StorageCapacityIncrements",
-        "TreatmentCapacityIncrements",
-        "TreatmentEfficiency",
-        "RemovalEfficiency",
-        "DisposalExpansionCost",
-        "StorageExpansionCost",
-        "TreatmentExpansionCost",
-        "PipelineCapexDistanceBased",
-        "PipelineCapexCapacityBased",
-        "PipelineCapacityIncrements",
-        "PipelineExpansionDistance",
-        "Hydraulics",
-        "Economics",
-        "ExternalWaterQuality",
-        "PadWaterQuality",
-        "StorageInitialWaterQuality",
-        "PadStorageInitialWaterQuality",
-        "DisposalOperatingCapacity",
-        "TreatmentExpansionLeadTime",
-        "DisposalExpansionLeadTime",
-        "StorageExpansionLeadTime",
-        "PipelineExpansionLeadTime_Dist",
-        "PipelineExpansionLeadTime_Capac",
-        "SWDDeep",
-        "SWDAveragePressure",
-        "SWDProxPAWell",
-        "SWDProxInactiveWell",
-        "SWDProxEQ",
-        "SWDProxFault",
-        "SWDProxHpOrLpWell",
-        "SWDRiskFactors",
-    ]
-
-
 @pytest.mark.parametrize(
     "file_name",
     [
@@ -189,12 +27,12 @@ class Parameters:
     ],
 )
 def test_case_studies(file_name: str):
-    if file_name.startswith("operational"):
-        sets, parameters = Sets.operational, Parameters.operational
-    elif file_name.startswith("strategic"):
-        sets, parameters = Sets.strategic, Parameters.strategic
+    for mt in ["operational", "strategic"]:
+        if file_name.startswith(mt):
+            model_type = mt
+            break
 
     with resources.path("pareto.case_studies", file_name) as fpath:
         # changing to raises=True will cause the test to fail on data loading failures
-        data = get_data(fpath, sets, parameters, raises=False)
+        data = get_data(fpath, model_type=model_type raises=False)
     assert data is not None
